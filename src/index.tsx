898b661748299bd7f829ed7214095e7e96dc7957
kanounfoued <kanoun-foued@outlook.com>
2022-03-21 22:57:49 +0100

    :white_check_mark: make first test for app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import Header from './header';
 
 class Welcome extends React.Component {
